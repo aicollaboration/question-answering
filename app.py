031d805444198a98706f60751e322a097be2d901
Tobias Oberrauch <tobias.oberrauch@gmx.de>
2020-07-12 23:29:56 +0200

    Add algorithm

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import datetime
-from flask import Flask, jsonify, request, send_file
+from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
+from transformers import DistilBertTokenizer, DistilBertForQuestionAnswering
+import torch
 
 app = Flask(__name__)
 cors = CORS(app)
+
+tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased', return_token_type_ids=True)
+model = DistilBertForQuestionAnswering.from_pretrained('distilbert-base-uncased-distilled-squad')
+
 
 API_V1 = '/api/1.0'
 
@@ -16,7 +22,7 @@ def info():
     return jsonify({
         'version': API_V1,
         'project': '5 elements of AI',
-        'service': 'backend',
+        'service': 'question-answering',
         'language': 'python',
         'type': 'api',
         'date': str(datetime.datetime.now()),
@@ -26,7 +32,19 @@ def info():
 @app.route(API_V1 + '/predict', methods=['POST', 'OPTIONS'])
 @cross_origin(origin='localhost')
 def predict():
-    return 'no data provided'
+    data = request.json
+    
+    encoding = tokenizer.encode_plus(data['question'], data['context'])
+    input_ids, attention_mask = encoding["input_ids"], encoding["attention_mask"]
+    start_scores, end_scores = model(torch.tensor([input_ids]), attention_mask=torch.tensor([attention_mask]))
+
+    ans_tokens = input_ids[torch.argmax(start_scores) : torch.argmax(end_scores)+1]
+    answer_tokens = tokenizer.convert_ids_to_tokens(ans_tokens , skip_special_tokens=True)
+
+    all_tokens = tokenizer.convert_ids_to_tokens(input_ids)
+    answer_tokens_to_string = tokenizer.convert_tokens_to_string(answer_tokens)
+    
+    return answer_tokens_to_string
     
 
 if __name__ == "__main__":
